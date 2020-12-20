378bd6cf5a38928c073be4b926e2c6c06bbc9a35
Linus Zetterman <linuszetterman123@gmail.com>
2020-12-20 23:13:43 +0100

    movement

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -1,41 +1,45 @@
 import pygame
-
 pygame.init()
 
-direction = ""
+def start():
+    global direction, player, map, win, display
 
-player = {"x": 5, "y": 5, "vel": 1}
+    direction = "right"
+    player = {"x": 5, "y": 5, "vel": 1}
 
-win = pygame.display.set_mode((600, 400))
-display = pygame.Surface((30, 20))
+    map = {"width": 30, "height": 20}
+    win = pygame.display.set_mode((600, 400))
+    display = pygame.Surface((30, 20))
 
-
+start()
 run = True
 
 while run:
-    pygame.time.delay(200)
+    pygame.time.delay(150)
+    if player["x"] < 0 or player["x"] >= map["width"] or player["y"] < 0 or player["y"] >= map["height"]:
+        run = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         elif event.type == pygame.KEYDOWN:
-            if not direction == "left" or "right":
+            if direction == "up" or direction == "down":
                 if event.key == pygame.K_LEFT:
                     direction = "left"
                 elif event.key == pygame.K_RIGHT:
                     direction = "right"
-            if not direction == "up" or "down":
+            if direction == "left" or direction == "right":
                 if event.key == pygame.K_UP:
                     direction = "up"
                 elif event.key == pygame.K_DOWN:
                     direction = "down"
 
     if direction == "left":
-            player["x"] -= 1
+        player["x"] -= 1
     if direction == "right":
         player["x"] += 1
-    elif direction == "up":
+    if direction == "up":
         player["y"] -= 1
-    elif direction == "down":
+    if direction == "down":
         player["y"] += 1
 
     display.set_at((player["x"], player["y"]), (0, 255, 0))
